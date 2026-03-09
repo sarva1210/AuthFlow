818b0bf3fd966a9ef875323ec04dafc19e5e6c16
sarva1210 <sarva.vish77@gmail.com>
2026-03-10 04:12:25 +0530

    changes

---
+++ Backend/src/routes/auth.routes.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import { registerUser, loginUser, verifyEmail, getProfle } from '../controllers/auth.controller.js'
+import { protect } from '../middleware/auth.middleware.js'
+
+const router = express.Router()
+
+//Routes
+router.post("/register", registerUser)
+
+router.post("/login", loginUser)
+
+router.get("/verify/:token", verifyEmail)
+
+router.get("/profile", protect, getProfle)
+
+export default router
