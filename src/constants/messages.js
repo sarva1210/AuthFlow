51dce898d85acadb7574ae24a1a2062acc842f47
sarva1210 <sarva.vish77@gmail.com>
2026-03-10 04:13:38 +0530

    Restructured project folders

--- src/constants/messages.js
+++
@@ -1,9 +0,0 @@
-export const MESSAGES = {
-    USER_EXISTS:"User already exists",
-    USER_CREATED:"User registered successfully",
-    LOGIN_SUCCESS:"Login successfully",
-    INVALID_CREDENTIALS:"Invalid credentials",
-    VERIFY_EMAIL_FIRST:"Please verify your email first",
-    EMAIL_VERIFIED:"Email verified successfully",
-    UNAUTHORIZED:"Unauthorized access"
-}
--- src/utils/hashPassword.js
+++
@@ -1,9 +0,0 @@
-import bcrypt from 'bcrypt'
-
-export const hashPassword = async(password)=>{
-    return await bcrypt.hash(password,10)
-}
-
-export const comparePassword = async(password,hashed)=>{
-    return await bcrypt.compare(password, hashed)
-}
